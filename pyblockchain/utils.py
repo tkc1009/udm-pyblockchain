32966158d29eabe2d95292a4f23c92ddc3f4fad1
tkc1009 <tkc1009@users.noreply.github.com>
2023-02-17 17:29:54 +0900

    11. ナンスを導き出す

--- pyblockchain/utils.py
+++ pyblockchain/utils.py
@@ -1,6 +1,22 @@
 import collections
 import hashlib
 
+def pprint(chains):
+    print("START")
+    for i, chain in enumerate(chains):
+        print(f'{"="*25} Chain {i} {"="*25}')
+        for k, v in chain.items():
+            if k == 'transactions':
+                print(k)
+                for d in v:
+                    print(f'{"-"*40}')
+                    for kk, vv in d.items():
+                        print(f' {kk:30}{vv}')
+            else:
+                print(f'{k:15}{v}')
+        print(f'{"*"*60}')
+    print("FINISH")
+    
 def sorted_dict_by_key(unsorted_dict):
     return collections.OrderedDict(
         sorted(unsorted_dict.items(), key=lambda d:d[0]))
